c01ded83b905489963d714eef2dd51a675b49f14
Tuyet <tgdd.ld9941@gmail.com>
2025-04-02 10:00:02 +0700

    fix error

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def api_projects():
     return jsonify({"projects": PROJECTS, "personal_projects": PERSONAL_PROJECTS})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) 
+    port = int(os.environ.get('PORT', 5000))  
     app.run(host='0.0.0.0', port=port, threaded=True)
